ecdb6fd6ca0ad656a246b9146fb4a68258e2c2a6
Shiming Zhang <wzshiming@foxmail.com>
2021-08-19 21:11:13 +0800

    Support fallback

--- schedialer.go
+++ schedialer.go
@@ -2,22 +2,26 @@ package schedialer
 
 import (
 	"context"
+	"fmt"
 	"net"
+	"time"
 )
 
 func NewSchedialer(plugins *Plugins) *Schedialer {
 	return &Schedialer{
 		Plugins:  plugins,
 		Resolver: net.DefaultResolver,
+		Period:   time.Second,
 	}
 }
 
 type Schedialer struct {
 	Plugins  *Plugins
 	Resolver *net.Resolver
+	Period   time.Duration
 }
 
-func (s *Schedialer) Match(ctx context.Context, network, address string) (*Proxy, error) {
+func (s *Schedialer) Ranking(ctx context.Context, network, address string) ([]*Proxy, error) {
 	resolver := s.Resolver
 	if resolver == nil {
 		resolver = net.DefaultResolver
@@ -46,13 +50,35 @@ func (s *Schedialer) Match(ctx context.Context, network, address string) (*Proxy
 		IPs:     ips,
 		Port:    port,
 	}
-	return s.Plugins.Match(ctx, &target)
+	return s.Plugins.Ranking(ctx, &target)
 }
 
 func (s *Schedialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
-	proxy, err := s.Match(ctx, network, address)
+	proxies, err := s.Ranking(ctx, network, address)
 	if err != nil {
 		return nil, err
 	}
-	return proxy.Dialer.DialContext(ctx, network, address)
+
+	switch len(proxies) {
+	case 0:
+		return nil, fmt.Errorf("no proxy available")
+	case 1:
+		return proxies[0].Dialer.DialContext(ctx, network, address)
+	}
+	return fallbackDial(proxies, ctx, network, address)
+}
+
+func fallbackDial(proxies []*Proxy, ctx context.Context, network, address string) (net.Conn, error) {
+	var err error
+	for _, proxy := range proxies {
+		conn, e := proxy.Dialer.DialContext(ctx, network, address)
+		if e != nil {
+			if err == nil {
+				err = e
+			}
+			continue
+		}
+		return conn, nil
+	}
+	return nil, err
 }
